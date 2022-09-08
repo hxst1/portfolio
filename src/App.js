4f34fed3c4d2ce6bd85621f70ddb01a609b2b272
Edu Ruiz <ruizcantosps@gmail.com>
2022-09-08 15:19:23 +0200

    React router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Projects from "./components/projects/projects";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
      <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="projects" element={<Projects />} />
       </Routes>
   );
 }
